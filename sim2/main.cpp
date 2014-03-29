effd77cb260aabc5cc6e6d7cf16fb17c9a0ee441
Michal Zochniak <michal@zapu.net>
2014-03-29 22:06:50 +0100

    Optimization settings

--- sim2/main.cpp
+++ sim2/main.cpp
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include "tools.h"
 #include "system.h"
+#include <iostream>
 
 Project project;
 
 int main() {
-	for(int i = 0; i < 1000; i++) {
+	for(int i = 0; i < 100000; i++) {
 		auto job = new Job();
 		project.m_jobs.insert(job);
 	}
 
-	for(int i = 0; i < 100; i++) {
+	for(int i = 0; i < 1000; i++) {
 		auto node = new Node();
 		node->m_trust = randf();
 		project.addNode(node);
 	}
+
+	std::cout << "Simulating..." << std::endl;
 
 	project.simulate();
 }
